a394c22412eaeb0fffbaf4766e7ce7134521e2bb
MonomCZ <vitek.monom@gmail.com>
2026-09-14 09:11:00 +0200

    added a testing block of code

--- Functions/buttonsnkeyboardsim.py
+++ Functions/buttonsnkeyboardsim.py
@@ -4,7 +4,8 @@ from Functions.keyboard_simulation import type_string, press_key, setup_gadget
 def run():
     while True:
         if button.button1():
-            press_key(0x00,0x04) #a
+            press_key(0x08, 0x15) #testing
+            #press_key(0x00,0x04) #a
         if button.button2():
             press_key(0x16) #s
         if button.button4():
