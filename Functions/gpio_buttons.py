311db0709f89e1d2453eab7c23b8ff1d6b4d19ad
MonomCZ <vitek.monom@gmail.com>
2026-09-14 08:59:36 +0200

    removed debug code

--- Functions/gpio_buttons.py
+++ Functions/gpio_buttons.py
@@ -50,19 +50,20 @@ def button3():
         else:
             return False
 
-while True:
-    if button1():
-        print("Button 1 pressed")
-        time.sleep(0.5)
-    if button2():
-        print("Button 2 pressed")
-        time.sleep(0.5)
-    if button3():
-        print("Button 3 pressed")
-        time.sleep(0.5) 
-    if button4():
-        print("Button 4 pressed")
-        time.sleep(0.5)
-    if button5():
-        print("Button 5 pressed")
-        time.sleep(0.5)
+if __name__ == "__main__":
+    while True:
+        if button1():
+            print("Button 1 pressed")
+            time.sleep(0.5)
+        if button2():
+            print("Button 2 pressed")
+            time.sleep(0.5)
+        if button3():
+            print("Button 3 pressed")
+            time.sleep(0.5) 
+        if button4():
+            print("Button 4 pressed")
+            time.sleep(0.5)
+        if button5():
+            print("Button 5 pressed")
+            time.sleep(0.5)
