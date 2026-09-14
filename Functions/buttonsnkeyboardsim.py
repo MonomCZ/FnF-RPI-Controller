ad7862c205d3f03f45634b6f6affbd2d50a495c7
MonomCZ <vitek.monom@gmail.com>
2026-09-14 09:27:29 +0200

    removed the break

--- Functions/buttonsnkeyboardsim.py
+++ Functions/buttonsnkeyboardsim.py
@@ -4,16 +4,13 @@ from Functions.keyboard_simulation import type_string, press_key, setup_gadget
 def run():
     while True:
         if button.button1():
-            press_key(0x08, 0x15) #testing
-            #press_key(0x00,0x04) #a
-            break
+            press_key(0x00,0x04) #a
         if button.button2():
             press_key(0x16) #s
         if button.button4():
             press_key(0x00,0x0e) #k
         if button.button5():
             press_key(0x00,0x0f) #l
-        
 
 
 
