a608afa1396a817f28469dd349426ab0fa22ec39
MonomCZ <vitek.monom@gmail.com>
2026-09-14 09:04:19 +0200

    switched to a different function

--- Functions/buttonsnkeyboardsim.py
+++ Functions/buttonsnkeyboardsim.py
@@ -4,13 +4,13 @@ from Functions.keyboard_simulation import type_string, press_key, setup_gadget
 def run():
     while True:
         if button.button1():
-            type_string("a")
+            press_key(0x04) #a
         if button.button2():
-            type_string("s")
+            press_key(0x16) #s
         if button.button4():
-            type_string("k")
+            press_key(0x0e) #k
         if button.button5():
-            type_string("l")
+            press_key(0x0f) #l
 
 
 
